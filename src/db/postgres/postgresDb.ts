2505eb7d5b363cf53ba7153573d3b793c123a3b5
ukio <priz.a47@gmail.com>
2024-07-03 15:05:53 +0500

    fix

--- src/db/postgres/postgresDb.ts
+++ src/db/postgres/postgresDb.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 
-const sequelize:Sequelize = new Sequelize('postgres://gnom:g_nom@localhost:5432/gnom');
-// const sequelize:Sequelize = new Sequelize('postgres://postgres:1234@localhost:5432/gnom');
+//const sequelize:Sequelize = new Sequelize('postgres://gnom:g_nom@localhost:5432/gnom');
+const sequelize:Sequelize = new Sequelize('postgres://postgres:1234@localhost:5432/gnom',{logging:false});
     
 export default sequelize;
