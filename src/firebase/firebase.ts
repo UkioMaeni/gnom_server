a0d4c248fe4c4edfa6be9e394d077c0d8b0d389f
ukio <priz.a47@gmail.com>
2024-12-26 07:16:20 +0500

    add

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,12 +1,16 @@
 import admin from 'firebase-admin';
 import { Message } from 'firebase-admin/lib/messaging/messaging-api';
 var serviceAccount =require('./fboptions.json')
+
+   let app:admin.app.App|null=null;
+
 class FireBaseService{
     initialize(){
-        const app=admin.initializeApp({
+        app=admin.initializeApp({
             credential: admin.credential.cert(serviceAccount)
         });
         console.log(app.auth().app.options.credential);
+        
         
     }
     async sendNotification(fcm:string,messageType:string):Promise<void>{
@@ -21,7 +25,7 @@ class FireBaseService{
               messageType
             }
           };
-          await admin.messaging().send(message)
+          await app!.messaging().send(message)
         } catch (error) {
           console.log(error);
           
