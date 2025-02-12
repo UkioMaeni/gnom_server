6f39c7180f573fde750a683d8fbaeb0646396aec
ukio <priz.a47@gmail.com>
2025-02-12 12:08:03 +0500

    ass

--- src/server/httpsServer.ts
+++ src/server/httpsServer.ts
@@ -9,8 +9,8 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
 };
 
 
