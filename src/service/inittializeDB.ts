04512ac9ff9e1915d29d22f9274796aa720e6e05
ukio <priz.a47@gmail.com>
2025-03-22 06:55:43 +0500

    fix

--- src/service/inittializeDB.ts
+++ src/service/inittializeDB.ts
@@ -28,19 +28,24 @@ class InitializeDBService{
     }
 
     private async initializeModels(){
-        await PaymentTransactions.sync({ alter: true });
-        await SupportAccess.sync({alter:true})
-        await  User.sync({ alter: true });
-        await  Guest.sync({ alter: true });
-        await  Requests.sync({ alter: true });
-        await  GuestRequests.sync({ alter: true });
-        await  Subject.sync({ alter: true });
-        await  UserTokens.sync({ alter: true });
-        await  GuestTokens.sync({ alter: true });
-        await  Transaction.sync({ alter: true });
-        await  MailCode.sync({ alter: true });
-        await  FCM.sync({ alter: true });
-        await UnreadMessages.sync({ alter: true });
+        try {
+            await PaymentTransactions.sync({ alter: true });
+            await SupportAccess.sync({alter:true})
+            await  User.sync({ alter: true });
+            await  Guest.sync({ alter: true });
+            await  Requests.sync({ alter: true });
+            await  GuestRequests.sync({ alter: true });
+            await  Subject.sync({ alter: true });
+            await  UserTokens.sync({ alter: true });
+            await  GuestTokens.sync({ alter: true });
+            await  Transaction.sync({ alter: true });
+            await  MailCode.sync({ alter: true });
+            await  FCM.sync({ alter: true });
+            await UnreadMessages.sync({ alter: true });
+        } catch (error) {
+            console.log(error);
+            
+        }
         
     }
 }
